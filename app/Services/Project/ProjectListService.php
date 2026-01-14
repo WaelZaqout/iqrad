f83d326463489fc1bc825296cf878e1093744836
wael zaqout <94363862+WaelZaqout@users.noreply.github.com>
2026-01-14 14:50:32 +0200

    Update ProjectListService.php

--- app/Services/Project/ProjectListService.php
+++ app/Services/Project/ProjectListService.php
@@ -2,15 +2,15 @@
 
 namespace App\Services\Project;
 
-use App\Models\Category;
+use App\Models\Project;
 use Illuminate\Http\Request;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
-class CategoryListService
+class ProjectListService
 {
     public function execute(Request $request): LengthAwarePaginator
     {
-        return Category::query()
+        return Project::query()
             ->with(['parent'])
             ->when($request->filled('q'), function ($query) use ($request) {
                 $query->search($request->q);
